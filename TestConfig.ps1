15acf6c83276add3ee116314f7efcc38613a075a
erjosito <erjosito@hotmail.com>
2017-11-13 16:17:26 +0100

    script resource

--- TestConfig.ps1
+++ TestConfig.ps1
@@ -17,15 +17,25 @@ configuration TestConfig
             StartupType = 'Automatic'
             State = 'Running'
         }
-        File index
-        {            	
+        #File index
+        #{            	
             # Download an HTML home page
-            Ensure = "Present"
-            Type = "File"
-            SourcePath ="https://raw.githubusercontent.com/erjosito/DSC/master/index.html"
-            DestinationPath = "C:\inetpub\wwwroot\index.html"
-            MatchSource = $true
+            # net use Z: \\permanentlabdisks578.file.core.windows.net\myshare /u:AZURE\permanentlabdisks578 SQm+x0M2Vk4lLAatfLmIlS4ErI4fXC6JbPH9LevqgsU11Y/XM/dQg9gS+fCleyBDr51x+poYBalnXi1R7SOtZQ==
+        #    Ensure = "Present"
+        #    Type = "File"
+            #SourcePath ="https://raw.githubusercontent.com/erjosito/DSC/master/index.html"
+        #    SourcePath="\\permanentlabdisks578.file.core.windows.net\myshare\webfiles\index.html"
+        #    DestinationPath = "C:\inetpub\wwwroot\index.html"
+        #    Credential = $storageCredentials
+        #    MatchSource = $true
+        #}
+        Script myScript
+        {
+            GetScript = " $False "
+            SetScript = "Invoke-WebRequest -uri https://raw.githubusercontent.com/erjosito/DSC/master/index.html -outfile C:\inetpub\wwwroot\index.html" 
+            TestScript = " $False "
         }
+
     }
 
     Node myConfig2
